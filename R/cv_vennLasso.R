580892266dfec01f037b90d5f43ca670ecec33a3
jaredhuling <huling@wisc.edu>
2017-07-12 21:33:25 -0500

    more readability changes

--- R/cv_vennLasso.R
+++ R/cv_vennLasso.R
@@ -509,47 +509,57 @@ cv.vennbinomial <- function(outlist,lambda,x,y,groups,foldid,type.measure,groupe
 }
 
 cv.venngaussian <- function(outlist,lambda,x,y,groups,foldid,type.measure,grouped,keep=FALSE){
-  typenames=c(deviance="Mean-Squared Error",mse="Mean-Squared Error",mae="Mean Absolute Error")
-  if(type.measure=="default")type.measure="mse"
-  if(!match(type.measure,c("mse","mae","deviance"),FALSE)){
+  typenames <- c(deviance = "Mean-Squared Error",
+                 mse      = "Mean-Squared Error",
+                 mae      = "Mean Absolute Error")
+  if(type.measure == "default") type.measure <- "mse"
+  if(!match(type.measure,c("mse","mae","deviance"),FALSE))
+  {
     warning("Only 'mse', 'deviance' or 'mae'  available for Gaussian models; 'mse' used")
-    type.measure="mse"
+    type.measure <- "mse"
   }
   #if(!is.null(offset))y=y-drop(offset)
-  predmat=matrix(NA,length(y),length(lambda))
-  nfolds=max(foldid)
-  nlams=double(nfolds)
-  for(i in seq(nfolds)){
-    which=foldid==i
-    fitobj=outlist[[i]]
+  predmat <- matrix(NA,length(y),length(lambda))
+  nfolds  <- max(foldid)
+  nlams   <- double(nfolds)
+  for(i in seq(nfolds))
+  {
+    which    <- foldid == i
+    fitobj   <- outlist[[i]]
     #fitobj$offset=FALSE
-    preds=predict(fitobj,x[which,,drop=FALSE], group.mat = groups[which,,drop=FALSE], type="response")
-    nlami=length(outlist[[i]]$lambda)
-    predmat[which,seq(nlami)]=preds
-    nlams[i]=nlami
+    preds    <- predict(fitobj, x[which,,drop=FALSE], group.mat = groups[which,,drop=FALSE], type = "response")
+    nlami    <- length(outlist[[i]]$lambda)
+    predmat[which,seq(nlami)] <- preds
+    nlams[i] <- nlami
   }
 
-  N=length(y) - apply(is.na(predmat),2,sum)
-  cvraw=switch(type.measure,
-               "mse"=(y-predmat)^2,
-               "deviance"=(y-predmat)^2,
-               "mae"=abs(y-predmat)
+  N <- length(y) - apply(is.na(predmat),2,sum)
+  cvraw <- switch(type.measure,
+                  "mse"=(y-predmat)^2,
+                  "deviance"=(y-predmat)^2,
+                  "mae"=abs(y-predmat)
   )
-  if( (length(y)/nfolds <3)&&grouped){
+  if( (length(y)/nfolds <3) && grouped)
+  {
     warning("Option grouped=FALSE enforced in cv.glmnet, since < 3 observations per fold",call.=FALSE)
-    grouped=FALSE
+    grouped <- FALSE
   }
-  if(grouped){
-    cvob=cvcompute(cvraw,rep(1, length(y)),foldid,nlams)
-    cvraw=cvob$cvraw;weights=cvob$weights;N=cvob$N
+  if(grouped)
+  {
+    cvob    <- cvcompute(cvraw,rep(1, length(y)),foldid,nlams)
+    cvraw   <- cvob$cvraw
+    weights <- cvob$weights
+    N       <- cvob$N
   }
 
   #cvm=apply(cvraw,2,weighted.mean,w=weights,na.rm=TRUE)
-  cvm=apply(cvraw,2,mean,na.rm=TRUE)
+  cvm  <- apply(cvraw,2,mean,na.rm=TRUE)
   #cvsd=sqrt(apply(scale(cvraw,cvm,FALSE)^2,2,weighted.mean,w=weights,na.rm=TRUE)/(N-1))
-  cvsd=sqrt(apply(scale(cvraw,cvm,FALSE)^2,2,mean,na.rm=TRUE)/(N-1))
-  out=list(cvm=cvm,cvsd=cvsd,name=typenames[type.measure])
-  if(keep)out$fit.preval=predmat
+  cvsd <- sqrt(apply(scale(cvraw,cvm,FALSE)^2, 2, mean, na.rm = TRUE)/(N-1))
+  out  <- list(cvm  = cvm,
+               cvsd = cvsd,
+               name = typenames[type.measure])
+  if(keep)out$fit.preval <- predmat
   out
 }
 
